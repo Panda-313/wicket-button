3cd2cdbb204b6a265a7c940c7835a67449493412
mikolaj <mikolaj.cieslinski@herodot.com>
2021-07-16 13:41:05 +0200

    change redirect uri

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,6 @@ export class AppComponent {
   constructor(private httpClient: HttpClient) {
   }
   registerByGoogle(): void {
-    this.httpClient.get('https://legia-dev-profile.auth.eu-central-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=1qqhfsaqrsd6e1u7at200mg8s2&redirect_uri=http://localhost:4200/rejestracja&state=STATE&scope=openid+profile+aws.cognito.signin.user.admin&identity_provider=Google').subscribe(res => console.log(res))
+    this.httpClient.get('https://legia-dev-profile.auth.eu-central-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=1qqhfsaqrsd6e1u7at200mg8s2&redirect_uri=https://zealous-hermann-3c28f5.netlify.app&state=STATE&scope=openid+profile+aws.cognito.signin.user.admin&identity_provider=Google').subscribe(res => console.log(res))
   }
 }
